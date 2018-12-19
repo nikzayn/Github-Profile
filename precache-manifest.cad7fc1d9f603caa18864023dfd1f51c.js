f8dd4ad2c73ef28e1d5eed62e6d105d22a2460f7
Nikhil <nikhilvaidyar1997@gmail.com>
2018-12-19 22:11:44 +0530

    Updates

---
+++ precache-manifest.cad7fc1d9f603caa18864023dfd1f51c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34f38a83000e18d572e7",
+    "url": "/myapp/static/css/main.5bea0363.chunk.css"
+  },
+  {
+    "revision": "34f38a83000e18d572e7",
+    "url": "/myapp/static/js/main.34f38a83.chunk.js"
+  },
+  {
+    "revision": "af839bbb27421718b35a",
+    "url": "/myapp/static/js/1.af839bbb.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "20643773bc684d94d8e842b9bbea533c",
+    "url": "/myapp/index.html"
+  }
+];
